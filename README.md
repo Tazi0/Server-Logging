b582247ecf238b112a818f23e36174d7cf3e615a
Tazio <tazio2003@gmail.com>
2019-05-07 21:35:40 +0200

    v1.2 Big update
    -Discord webhook active message
    -Discord webhook now has players icon (steam api key required)
    -Discord webhook now has color
    -Discord webhook System icon (default icon is FiveM)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Server Logging
+A FiveM Plugin
+
+# Installation
+1. Download `Serverlogging`!
+2. Install it into `/resources/`.
+3. Enter your webhook url in `server.lua`
+4. Make a Steam API Key in https://steamcommunity.com/dev/apikey and paste the key in `server.lua`
+5. Start the plugin
+6. Have fun!
+
+For more information goto:
+https://forum.fivem.net/t/server-log-discord-webhook/482062
--- Serverlogging/server.lua
+++ Serverlogging/server.lua
@@ -2,40 +2,72 @@
 
 local DISCORD_WEBHOOK = ""
 local DISCORD_NAME = "System"
--- local DISCORD_IMAGE = "" !coming soon!
+local STEAM_KEY = ""
+local DISCORD_IMAGE = "https://pbs.twimg.com/profile_images/847824193899167744/J1Teh4Di_400x400.jpg" -- default is FiveM logo
 
 --DON'T EDIT BELOW THIS
 
-
---PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = "**Serverlogging is ONLINE**"}), { ['Content-Type'] = 'application/json' })
-
+PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = "**Discord Webhook is ONLINE**"}), { ['Content-Type'] = 'application/json' })
 
 AddEventHandler('chatMessage', function(source, name, message) 
-    PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = name .. " [" .. source .. "]", content = message}), { ['Content-Type'] = 'application/json' })
+	--print(tonumber(GetIDFromSource('steam', source), 16)) -- DEBUGGING
+	--print('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' .. STEAM_KEY .. '&steamids=' .. tonumber(GetIDFromSource('steam', source), 16))
+	if STEAM_KEY == '' or STEAM_KEY == nil then
+		PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = name .. " [" .. source .. "]", content = message}), { ['Content-Type'] = 'application/json' })
+	else
+		PerformHttpRequest('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' .. STEAM_KEY .. '&steamids=' .. tonumber(GetIDFromSource('steam', source), 16), function(err, text, headers)
+			local image = string.match(text, '"avatarfull":"(.-)","')
+			--print(image) -- DEBUGGING
+			return PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = name .. " [" .. source .. "]", content = message, avatar_url = image, tts = false}), { ['Content-Type'] = 'application/json' })
+		end)
+	end
 end)
 
 AddEventHandler('playerConnecting', function() 
-    PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = "```".. GetPlayerName(source) .. " connecting ```"}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = "```CSS\n".. GetPlayerName(source) .. " connecting\n```", avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
 end)
 
 AddEventHandler('playerDropped', function(reason) 
-    PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = "```".. GetPlayerName(source) .. " left ( ".. reason .. " )```"}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = "```fix\n".. GetPlayerName(source) .. " left ( ".. reason .. " )\n```", avatar_url = DISCORD_IMAGE}), { ['Content-Type'] = 'application/json' })
 end)
 
 RegisterServerEvent('playerDied')
 AddEventHandler('playerDied',function(killer,reason)
-	if killer == "**Invalid**" then --Can't figure out what's generating invalid, it's late. If you figure it out, let me know. I just handle it as a string for now.
+	if killer == "**Invalid**" then
 		reason = 2
 	end
 	if reason == 0 then
-		--TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ committed suicide. ")
     	PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = GetPlayerName(source) .. " committed suicide"}), { ['Content-Type'] = 'application/json' })
 	elseif reason == 1 then
-		--TriggerClientEvent('showNotification', -1,"~o~".. killer .. "~w~ killed ~o~"..GetPlayerName(source).."~w~.")
     	PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = GetPlayerName(source) .. " was killed by: " .. killer}), { ['Content-Type'] = 'application/json' })
 	else
-		--TriggerClientEvent('showNotification', -1,"~o~".. GetPlayerName(source).."~w~ died.")
     	PerformHttpRequest(DISCORD_WEBHOOK, function(err, text, headers) end, 'POST', json.encode({username = DISCORD_NAME, content = GetPlayerName(source) .. " died"}), { ['Content-Type'] = 'application/json' })
 
 	end
 end)
+
+function GetIDFromSource(Type, ID) --(Thanks To WolfKnight [forum.FiveM.net])
+    local IDs = GetPlayerIdentifiers(ID)
+    for k, CurrentID in pairs(IDs) do
+        local ID = stringsplit(CurrentID, ':')
+        if (ID[1]:lower() == string.lower(Type)) then
+            return ID[2]:lower()
+        end
+    end
+    return nil
+end
+
+function stringsplit(input, seperator)
+	if seperator == nil then
+		seperator = '%s'
+	end
+	
+	local t={} ; i=1
+	
+	for str in string.gmatch(input, '([^'..seperator..']+)') do
+		t[i] = str
+		i = i + 1
+	end
+	
+	return t
+end
