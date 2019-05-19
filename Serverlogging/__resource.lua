cac14df04da02654fa94300f938b971e52da00a7
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-05-19 14:47:19 +0200

    Cleaned up
    Cleaned up

--- Serverlogging/__resource.lua
+++ Serverlogging/__resource.lua
@@ -1,4 +1,10 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Server Logging'
+
+version '1.0'
+
 server_script 'server.lua'
 client_script 'client.lua'
 
-description 'Server Logging'
+--edit and cleand up by NebelRebell
