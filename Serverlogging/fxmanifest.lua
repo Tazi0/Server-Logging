0b61d330f373db8ea2f2a9cf192d6aa25179a9b2
Luis <hypnoticsiege@gmail.com>
2022-01-13 08:15:06 -0500

    :bee: Updated to FX Manifest

---
+++ Serverlogging/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Tazio & HypnoticSiege'
+description 'A simple FiveM logging solution.'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
